4775ba4c89699b524568473e95aa9fe9c5978fa3
tridat2004 <161856417+tridat2004@users.noreply.github.com>
2024-05-27 21:50:35 +0700

    Dat update html,css,js

---
+++ js123.js/header.js
@@ -0,0 +1,9 @@
+document.getElementById("shopLink").addEventListener("click",function(event){
+    event.preventDefault();
+    window.location.href="page3.html"
+})
+
+document.getElementById("aboutLink").addEventListener("click",function(event){
+    event.preventDefault();
+    window.location.href="page2.html"
+})
